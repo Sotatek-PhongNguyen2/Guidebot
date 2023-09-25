6c827ecc78ec35e3289aa94da3a897abea5b209f
sherwind <doanphong.1413@gmail.com>
2023-09-25 08:08:23 +0700

    home page 90%

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,26 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        xl: '1280px',
+      },
+    },
+    letterSpacing: {
+      '1': '0em',
+      '2': '0.025em',
+      '3': '0.05em',
+      '4': '0.1em',
+      '5': '0.25em',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'teal-gradient':
+          'linear-gradient(139deg, #00879b 4.96%, #5de8d7 100.3%)',
       },
     },
   },
